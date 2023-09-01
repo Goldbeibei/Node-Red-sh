8df2943959736e262be5ae31d6ca517848c1ef39
Goldbeibei <102613276+Goldbeibei@users.noreply.github.com>
2023-09-01 12:23:01 +0800

    Update install_nodered.sh

--- install_nodered.sh
+++ install_nodered.sh
@@ -2,7 +2,9 @@
 #install node-red
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install nodejs -y
-sudo apt install npm -y
-sudo npm install -g --unsafe-perm node-red
-node-red
+sudo apt install git -y
+git clone https://github.com/node-red/node-red.git
+cd node-red
+npm install
+npm run build
+npm start
